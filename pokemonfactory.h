06da6f1ebf51ab427432ae1aaa793aa8296bc685
RainING1947 <934109129@qq.com>
2016-10-11 08:12:15 +0800

    miss PokemonFctory commit

---
+++ pokemonfactory.h
@@ -0,0 +1,15 @@
+﻿#ifndef POKEMONFACTORY_H
+#define POKEMONFACTORY_H
+
+#include "pokemon.h"
+#include "widget.h"
+
+class PokemonFactory
+{
+public:
+    PokemonFactory(){}
+    ~PokemonFactory(){}
+    Pokemon *CreatePokemon(Kind kind, int level);
+};
+
+#endif // POKEMONFACTORY_H
